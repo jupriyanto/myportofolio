183303946f5132ae2e9073ed69490153e1e32283
jupriyanto <boemel1631@gmail.com>
2023-10-12 10:40:14 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
